3b7f8ff40f68eb471613da3117ada9a5736b461b
devmahin <mahinhowlader123@gmail.com>
2024-02-22 23:23:09 +0600

    class object

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 // nested Object
 const product = {
-    naem : "Shart",
-    data : [
-        {id: 1, name : "shart 1 "},
-        {id: 2, name : "T shart 1"}
+    naem: "Shart",
+    data: [
+        { id: 1, name: "shart 1 " },
+        { id: 2, name: "T shart 1" }
     ]
 }
 // console.log(product.data[0])
 
 
-let number = [2,3,4,5,6,7]
+let number = [2, 3, 4, 5, 6, 7]
 // console.log(number)
 // for(let i=0; i<number.length; i++){
-    //     console.log(number[i])
-    // }
+//     console.log(number[i])
+// }
 // console.log(number)
 
 
@@ -26,7 +26,7 @@ let num = number.map(v => v * 5)
 
 let str = ["Mahin", "Alu", "Sakil", "Mula"]
 
-for(let i = 0; i<str.length; i++){
+for (let i = 0; i < str.length; i++) {
     let name = str[i]
     let first = name.charAt(0);
     // console.log(first)
@@ -35,15 +35,102 @@ for(let i = 0; i<str.length; i++){
 
 
 
-// let string = number.forEach(value => {
-//     return value * 5
-// })
+
+
+// let filters = fil.filter(val => val > 50)
+// // console.log(filters)
+
+// let finds = fil.find(val => val > 10);
+// // console.log(finds)
+
+
+
+
+let string = number.forEach(value => {
+    return value * 5
+})
 // console.log(string)
 
 
-let fil = [34,45,6,56,7]
-let filters = fil.filter(val => val > 50)
-// console.log(filters)
+let mahin = [1, 2, 3, 4, 5];
+let reduces = mahin.reduce((p, c) => p + c)
+// console.log(reduces)
 
-let finds = fil.find(val => val > 10);
-// console.log(finds)
+const top10Fruits = [
+    { 
+        id: 1,
+        name: "Durian",
+        price: 20 // price in dollars per kg
+    },
+    { 
+        id: 2,
+        name: "Saffron",
+        price: 11 // price in dollars per gram
+    },
+    {
+        id: 3,
+        name: "Gold Apple",
+        price: 5 // price in dollars per piece
+    },
+    {
+        id: 4,
+        name: "Pitahaya",
+        price: 7 // price in dollars per piece
+    },
+    {
+        id: 5,
+        name: "Mango",
+        price: 3 // price in dollars per kg
+    },
+    {
+        id: 6,
+        name: "Avocado",
+        price: 2.50 // price in dollars per piece
+    },
+    {
+        id: 7,
+        name: "Lychee",
+        price: 8 // price in dollars per kg
+    },
+    {
+        id: 8,
+        name: "Pomegranate",
+        price: 4 // price in dollars per kg
+    },
+    {
+        id: 9,
+        name: "Blueberry",
+        price: 6 // price in dollars per kg
+    },
+    {
+        id: 10,
+        name: "Dragon Fruit",
+        price: 5 // price in dollars per piece
+    }
+];
+
+// console.log(top10Fruits);
+let productName = top10Fruits.map(arr => arr.name)
+console.log(productName)
+
+top10Fruits.forEach(id => console.log(id.id))
+// console.log(filtes22)
+
+
+
+let find22 = top10Fruits.find(value => value.price > 5)
+console.log(find22)
+
+
+
+// let sre22 = number.forEach(value => {
+//     return value * 5
+// })
+let reduce22 = top10Fruits.reduce((pre, curr) => 
+    pre + curr.price,0)
+console.log(reduce22)
+
+
+
+
+
